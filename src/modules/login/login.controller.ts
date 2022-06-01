ca43a6c467fa6c3481e3f66c618bc864b284bea0
Oscar Manuel Bravo Carbajal <oscarmbravoc@gmail.com>
2022-06-01 01:30:25 -0500

    changes for api auth

--- src/modules/login/login.controller.ts
+++ src/modules/login/login.controller.ts
@@ -1,18 +1,18 @@
 import { Controller, Post, Res,  Body, Get, Param, Put } from '@nestjs/common';
 import { AuthService } from './service/auth/auth.service';
 
-@Controller('login')
+@Controller('auth')
 export class LoginController {
     constructor(private authService: AuthService) {}
     
-    @Post('filter')
+    @Post('login')
     async filterInfoPlaca(@Res() res, @Body() payload: any){
         const {username, password} = payload;
         const response = await this.authService.validateUserCredentials(username, password)
         return res.status(response.status).json(response.body);
     }
 
-    @Get('all')
+    @Get('user')
     async getAllInfoPlaca(@Res() res){
         const response = await this.authService.createUser()
         return res.status(200).json({status:true});
